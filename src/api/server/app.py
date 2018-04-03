6d4868aebf4f5c166c6daf2a32b328bdac437be4
zubant <ya.zubarevanton@yandex.ru>
2018-04-03 14:30:58 +0300

    example app

--- src/api/server/app.py
+++ src/api/server/app.py
@@ -1,5 +1,4 @@
 from sanic import Sanic
-from sanic.request import Request
 from sanic.response import json
 import logging
 
@@ -8,14 +7,9 @@ app = Sanic()
 logging.getLogger().setLevel(logging.INFO)
 
 
-@app.middleware('request')
-async def halt_request(request: Request):
-    logging.info(f"request: {request}")
-
-
 @app.route('/')
 async def test(request):
     return json({'hello': 'world'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False, access_log=False)
+    app.run(host='0.0.0.0', port=8000, debug=False, access_log=False, workers=4)
---
+++ src/api/server/flask_app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from sanic.response import json
+
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'hello'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000, debug=False)
