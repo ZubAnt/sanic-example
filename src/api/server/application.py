10173e1fde455068f88fe645180de1b305e71346
zubant <ya.zubarevanton@yandex.ru>
2018-04-03 15:36:17 +0300

    example app

--- src/api/server/application.py
+++ src/api/server/application.py
@@ -1,5 +1,5 @@
-from flask import Flask
 from injector import inject, singleton
+from sanic import Sanic
 
 from server.blueprints.default_blueprint import DefaultBlueprint
 from server.ioc import ioc
@@ -11,6 +11,6 @@ class Application(object):
     def __init__(self) -> None:
         self._default = ioc.get(DefaultBlueprint, scope=singleton).blueprint
 
-    def register(self, app: Flask) -> None:
-        app.register_blueprint(self._default, url_prefix='/default')
+    def register(self, app: Sanic) -> None:
+        app.blueprint(self._default, url_prefix='/default')
 
