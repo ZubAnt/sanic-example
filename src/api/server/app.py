9bb9c4c480ed8b2a4bc7e1e0296c3d1b93d3ea0c
zubant <ya.zubarevnton@yandex.ru>
2018-04-06 14:17:55 +0300

    run with gunicorn

--- src/api/server/app.py
+++ src/api/server/app.py
@@ -1,14 +1,83 @@
+import sys
+
+import sanic
 from sanic import Sanic
 import logging
 
 from server.application import Application
+from sanic.log import access_logger
 
-logging.getLogger().setLevel(logging.ERROR)
 
 
-app = Sanic()
+LOGGING_CONFIG_DEFAULTS = dict(
+    version=1,
+    disable_existing_loggers=False,
+
+    loggers={
+        "root": {
+            "level": "INFO",
+            "handlers": ["console"]
+        },
+        "sanic.error": {
+            "level": "INFO",
+            "handlers": ["error_console"],
+            "propagate": True,
+            "qualname": "sanic.error"
+        },
+
+        "sanic.access": {
+            "level": "ERROR",
+            "handlers": ["access_console"],
+            "propagate": True,
+            "qualname": "sanic.access"
+        }
+    },
+    handlers={
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "generic",
+            "stream": sys.stdout
+        },
+        "error_console": {
+            "class": "logging.StreamHandler",
+            "formatter": "generic",
+            "stream": sys.stderr
+        },
+        "access_console": {
+            "class": "logging.StreamHandler",
+            "formatter": "access",
+            "stream": sys.stdout
+        },
+    },
+    formatters={
+        "generic": {
+            "format": "%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter"
+        },
+        "access": {
+            "format": "%(asctime)s - (%(name)s)[%(levelname)s][%(host)s]: " +
+                      "%(request)s %(message)s %(status)d %(byte)d",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter"
+        },
+    }
+)
+
+
+# logging.getLogger('sanic.access').setLevel(logging.ERROR)
+# log_config = logging.basicConfig(level=logging.ERROR)
+# sanic.log.access_logger.setLevel(level=logging.ERROR)
+
+# logging.getLogger('sanic.access').setLevel(logging.ERROR)
+
+app = Sanic('test', log_config=LOGGING_CONFIG_DEFAULTS)
+logging.error('the matrix!')
+
 application = Application()
 application.register(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False, access_log=False, workers=1)
+    app.run(host='0.0.0.0', port=8000)
+
+
